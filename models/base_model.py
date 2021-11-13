74f37fed0dda04bced090e0522d6a06fb21e034d
Andres Diaz <andres.diaz@foonkiemonkey.co.uk>
2021-11-13 11:33:12 -0500

    Fix 4

--- models/base_model.py
+++ models/base_model.py
@@ -28,7 +28,7 @@ class BaseModel:
 
     def __str__(self):
         """Return a string of the class BaseModel"""
-        return "[{}]({}) {}".\
+        return "[{}] ({}) {}".\
             format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
