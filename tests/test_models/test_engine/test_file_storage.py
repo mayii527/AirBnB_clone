ef4f967b7a5da27dab324fd22a990a799694e242
Andres Diaz <andres.diaz@foonkiemonkey.co.uk>
2021-11-13 15:55:12 -0500

    fix 5

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -13,3 +13,22 @@ class TestFileStorage(unittest.TestCase):
         """Test if test_object is from the class FileStorage"""
         test_object = FileStorage()
         self.assertIsInstance(test_object, FileStorage)
+
+    def test_path_atribute(self):
+        """Test if is a valid path"""
+        self.assertTrue(FileStorage._FileStorage_file_path, "file.json")
+        self.assertEqual(FileStorage._FileStorage_file_path != 0)
+        self.assertTrue(type(FileStorage._FileStorage_file_path) is str)
+
+    def test_class_obj(self):
+        """Test if objects is dict"""
+        self.assertTrue(type(FileStorage._FileStorage_objects) is dict)
+
+    def test_all(self):
+        """test if all returns dict"""
+        test_object = models.storage.all()
+        self.assertIsInstance(test_object, dict)
+
+
+if __name__ == "__main__":
+    unittest.main()
