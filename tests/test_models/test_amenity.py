d8da5154dd891447ac5ed95bca604474a32af7fc
mayii527 <3434@holbertonschool.com>
2021-11-14 01:07:31 -0500

    add new tests cases

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -11,39 +11,60 @@ class TestAmenity(unittest.TestCase):
 
     def setUp(self):
         """creating object of Amenity"""
-        self.Amenity = Amenity()
+        self.Amenitys = Amenity()
 
     def exist_Amenity(self):
         """test of exist the class Amenity"""
-        self.assertEqual('[Amenity]' in str(self.Amenity), True)
+        self.assertEqual('[Amenity]' in str(self.Amenitys), True)
 
     def test_type(self):
         """test type of Amenity"""
-        self.assertEqual(type(self.Amenity), Amenity)
+        self.assertEqual(type(self.Amenitys), Amenity)
 
     def test_Amenity_id(self):
         """test id for Amenity"""
-        self.assertEqual(type(self.Amenity.id), str)
-        self.assertTrue(hasattr(self.Amenity, "id"))
+        self.assertEqual(type(self.Amenitys.id), str)
+        self.assertTrue(hasattr(self.Amenitys, "id"))
 
     def test_Amenity_created(self):
         """test created_ad for Amenity"""
-        self.assertEqual(type(self.Amenity.created_at), type(datetime.now()))
-        self.assertTrue(hasattr(self.Amenity, "created_at"))
+        self.assertEqual(type(self.Amenitys.created_at), type(datetime.now()))
+        self.assertTrue(hasattr(self.Amenitys, "created_at"))
 
     def test_Amenity_update(self):
         """test update_at for Amenity"""
-        self.assertEqual(type(self.Amenity.updated_at), type(datetime.now()))
-        self.assertFalse(hasattr(self.Amenity, "update_at"))
+        self.assertEqual(type(self.Amenitys.updated_at), type(datetime.now()))
+        self.assertFalse(hasattr(self.Amenitys, "update_at"))
 
     def test_method_str(self):
         """test str"""
-        self.assertEqual(type(str(self.Amenity)), str)
+        self.assertEqual(type(str(self.Amenitys)), str)
 
     def test_Amenity_name(self):
         """test name in Amenity"""
-        self.assertEqual(type(self.Amenity.name), str)
-        self.assertTrue(hasattr(self.Amenity, "name"))
+        self.assertEqual(type(self.Amenitys.name), str)
+        self.assertTrue(hasattr(self.Amenitys, "name"))
+
+    def test_strmethod_id(self):
+        '''Tests if id is type str'''
+        self.assertEqual('id' in str(self.Amenitys), True)
+
+    def test_strmethod_created(self):
+        """test if created_at type str"""
+        self.assertEqual('created_at' in str(self.Amenitys), True)
+
+    def test_srtmethod_update(self):
+        """test if update_at type str"""
+        self.assertEqual('update_at' in str(self.Amenitys), False)
+
+    def test_str_output(self):
+        '''Tests for output expected'''
+        output = "[{}] ({}) {}".format(
+            self.Amenitys.__class__.__name__,
+            self.Amenitys.id,
+            self.Amenitys.__dict__
+        )
+        self.assertEqual(output, str(self.Amenitys))
 
 if __name__ == "__main__":
     unittest.main()
